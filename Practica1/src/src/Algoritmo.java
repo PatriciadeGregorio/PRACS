a5a79d1754b64a619fb51a4421bfece9c273caa4
Agus <agustinschuler@hotmail.com>
2017-10-06 17:25:44 +0200

    jygjhjh

---
+++ Practica1/src/src/Algoritmo.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Algoritmo {
+
+	public static boolean[] selecActividadesOrdDecreInicio (int[] c, int[] f) {
+		boolean[] s = new boolean[c.length];
+		s[0] = true;
+		int i = 0;
+		for (int j=1; j<c.length; j++) {
+			if (c[i] >= f[j] || c[j] <= f[i]) {
+				s[j] = true;
+				i = j;
+			} else {
+				s[j] = false;
+			}
+		}
+		return s;
+	}
+}
